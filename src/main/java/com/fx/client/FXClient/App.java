cbb1bf4a00c35c6c6221e5c74abcede6aad7134d
Korhan <korhankilinckaya@gmail.com>
2016-09-19 20:15:46 +0300

    getting values from command prompt fix

--- src/main/java/com/fx/client/FXClient/App.java
+++ src/main/java/com/fx/client/FXClient/App.java
@@ -17,9 +17,12 @@ public class App
     {
   	  try {
 
-  		String type = "spot";
+  		String type = args[0];
   		String style = "";
-		    
+  		if(args.length != 1 && args[1]!=null){
+  			style = args[1];
+  		}
+  		
   		URL url = new URL("http://localhost:8080/FXRestfulServer/data?type="+type+"&style="+style);
   		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
   		conn.setRequestMethod("GET");
